6ee6561c89a9690209f9145542bb887c9b538308
Badol-Hossain1 <badolhossain586@gmail.com>
2025-07-02 22:36:20 +0600

    feat: menu items page fully complated

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,16 +1,39 @@
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import Cover from '../Shared/Cover/Cover'
 import MenuIcon from '/menu/banner3.jpg'
-import PopularMenu from '../Home/PopularMenu/PopularMenu'
+import Dessert from '/menu/dessert-bg.jpeg'
+import Menus from '/menu/menu-bg.png'
+import Pizza from '/menu/pizza-bg.jpg'
+import Salad from '/menu/salad-bg.jpg'
+import Soup from '/menu/soup-bg.jpg'
+
+import useMenu from '../../hooks/useMenu'
+import SectionTitle from '../../components/sectionTitle/SectionTitle'
+import MenuCategory from '../menuCategory/MenuCategory'
 
 const Menu = () => {
+    const [menu] = useMenu()
+    const desserts = menu.filter((item) => item.category === 'dessert')
+    const pizzas = menu.filter((item) => item.category === 'pizza')
+    const salads = menu.filter((item) => item.category === 'salad')
+    const soups = menu.filter((item) => item.category === 'soup')
+    const offereds = menu.filter((item) => item.category === 'offered')
     return (
         <div>
             <Helmet>
                 <title>hello world</title>
             </Helmet>
             <Cover title="OUR MENU" img={MenuIcon} />
-           
+
+            <SectionTitle subHeading='Don"t Miss' heading='Today"s offer' />
+
+            <MenuCategory title="Foods" img={MenuIcon} items={offereds} />
+
+            <MenuCategory title="Dessert" img={Dessert} items={desserts} />
+
+            <MenuCategory title="Salad" img={Salad} items={salads} />
+            <MenuCategory title="Pizza " img={Pizza} items={pizzas} />
+            <MenuCategory title="Soup" img={Soup} items={soups} />
         </div>
     )
 }
--- src/pages/menuCategory/MenuCategory.jsx
+++ src/pages/menuCategory/MenuCategory.jsx
@@ -1,11 +1,18 @@
+import Cover from '../Shared/Cover/Cover'
+import MenuItem from '../Shared/MenuItem/MenuItem'
 
-
-const MenuCategory = () => {
+const MenuCategory = ({ items, title,img }) => {
     return (
-        <div>
-            
+        <div className='pt-8'>
+            {title && <Cover img={img} title="our menu"></Cover>}
+                <div className="grid md:grid-cols-2 mt-10 gap-4">
+                    {items.map((item) => (
+                        <MenuItem key={item._id} item={item}></MenuItem>
+                    ))}
+                </div>
+            )
         </div>
-    );
-};
+    )
+}
 
-export default MenuCategory;
+export default MenuCategory
